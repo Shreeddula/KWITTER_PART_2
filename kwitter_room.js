9aa88ca5b04d4fc2665127d5ebc03f635fa8ceb1
Shreeddula <96717617+Shreeddula@users.noreply.github.com>
2023-05-14 16:16:46 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAcEpfZ3RVRefWBDhcw_jL309Maek-ALyo",
+    authDomain: "kwitter-56405.firebaseapp.com",
+    databaseURL: "https://kwitter-56405-default-rtdb.firebaseio.com",
+    projectId: "kwitter-56405",
+    storageBucket: "kwitter-56405.appspot.com",
+    messagingSenderId: "354575097852",
+    appId: "1:354575097852:web:74af662a4ac648b2479c79"
+  };
+  
